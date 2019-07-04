934b277e64a92cda9f89f4393fdacfe5ddc7663f
MONIKA KUMARI <monika7537kr@gmail.com>
2019-07-04 15:52:43 +0530

    updating

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,50 @@ router.get('/', function(request, response) {
 	response.render('landing.hbs');
 });
 
+// All authentication related code
+
+router.get('/login', function(request, response) {
+	response.render('login.hbs');
+});
+
+router.post('/login', function(request, response) {
+	var DB = request.app.locals.DB;
+	var user = {
+		email: request.body.email,
+		password: request.body.password
+	};
+
+	DB.collection('users').findOne(user, function(error, user) {
+		if (error || !user) {
+			response.render('invalid-login.hbs');
+			return;
+		}
+
+		request.session.user = user;
+		response.redirect('/restaurants');
+	});
+});
+
+router.get('/signup', function(request, response) {
+	response.render('signup.hbs');
+});
+
+router.post('/signup', function(request, response) {
+	var DB = request.app.locals.DB;
+	var newUser = {
+		name: request.body.name,
+		email: request.body.email,
+		password: request.body.password
+	};
+
+	DB.collection('users').insertOne(newUser, function(error, data) {
+		response.redirect('/login');
+	});
+});
+
+router.get('/logout', function(request, response) {
+	request.session.user = null;
+	response.redirect('/login');
+});
+
 module.exports = router;
