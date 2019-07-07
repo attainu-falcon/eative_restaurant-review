4310a47a93f93e33e4f92728bc239439bed7127f
rahultalele <talele.rahul2010@gmail.com>
2019-07-07 12:25:40 +0530

    rest.hbs added

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Include all the packages/modules we need.
+var express = require('express');
+var mongo = require('mongodb');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+//for routers folder
+var restaurantRoutes = require('./routes/restaurants');
+var indexRoutes = require('./routes/index');
+
+
+// Create the app
+var app = express();
+
+// App configurations and settings.
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: false }));
+// Static folder
+app.use(express.static('public'));
+app.use(session({ secret: 'catkey', resave: false, saveUninitialized: false }));
+
+//for routers folder
+app.use('/', indexRoutes);
+app.use('/restaurants', restaurantRoutes);
+
+
+// Create the DB connection
+var DB;
+var DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/eativerepo';
+
+// Create a Mongo client
+var mongoClient = new mongo.MongoClient(DB_URL, { useNewUrlParser: true });
+mongoClient.connect(function(error) {
+	if (error) {
+		console.log('Error connecting to the database.');
+	} else {
+		console.log('DB connection established.');
+		DB = mongoClient.db('eativerepo');
+		app.locals.DB = DB;
+	}
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ routes/index.js
@@ -0,0 +1,87 @@
+'use strict';
+var express = require('express');
+
+var router = express.Router();
+
+var mongo = require('mongodb');
+
+
+var bodyParser = require('body-parser');
+
+var session = require('express-session');
+
+
+// App routes (URLs)
+//main landing page with just logo
+router.get('/', function(request, response) {
+	response.render('landing.hbs');
+});
+
+// All authentication related code
+
+router.get('/login', function(request, response) {
+	response.render('login.hbs');
+});
+
+router.post('/login', function(request, response) {
+	var DB = request.app.locals.DB;
+	var user = {
+		email: request.body.email,
+		password: request.body.password
+	};
+
+	DB.collection('users').findOne(user, function(error, user) {
+		if (error || !user) {
+			response.render('invalid-login.hbs');
+			return;
+		}
+
+		request.session.user = user;
+		response.redirect('/restaurants');
+	});
+});
+
+router.get('/signup', function(request, response) {
+	response.render('signup.hbs');
+});
+
+router.post('/signup', function(request, response) {
+	
+	var newUser = {
+		name: request.body.name,
+		email: request.body.email,
+		password: request.body.password
+	};
+
+	DB.collection('users').insertOne(newUser, function(error, data) {
+		response.redirect('/login');
+	});
+});
+
+router.get('/logout', function(request, response) {
+	request.session.user = null;
+	response.redirect('/login');
+});
+
+router.get('/restaurants',function(request,response){
+	var DB = request.app.locals.DB;
+	DB.collection('restaurants').find().toArray(function(err, restaurants)
+	 { 
+		 if(err) 
+		 {
+			 throw err;
+		}
+		restaurants.sort(function(a, b){
+			return a.avgRating-b.avgRating
+		});
+		restaurants.reverse();
+		var results={
+			restaurants : restaurants
+		}
+		
+		console.log(restaurants);
+		response.render('restaurants.hbs',results);
+	} )
+})
+
+module.exports = router;
